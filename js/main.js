32d0542b7ff3ef970663148fe57b4ea093742f52
acho <achoegbunu@gmail.com>
2018-07-08 16:56:49 -0700

    answered question 1-12 on week 7 exercises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-[1,2,3].length * [“string”].length
+var myH1 = document.getElementById("myH1");
+	myH1.innerHTML = "some text!";
+var myNewH1 = document.getElementById("myNewH1")	
+
+
+
+function getInfo () {
+var value =	document.getElementById("userInfo").value;
+	console.log(value);
+	myNewH1.innerHTML = value;
+}
+
+var stringArray = ["coolest", "string", "array", "out", "there"];
+
+for (var i = 0; i < stringArray.length; i++) {
+ 	console.log(stringArray[i]);
+ } 
+
+ var myObj = {
+ 	name: "acho",
+ 	age: 34,
+ 	isCool: true
+
+ }
+
+ 
+ 
